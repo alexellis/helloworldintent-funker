833f40c883bdb05515b4f6ba595741c889bd6119
Alex Ellis <alexellis2@gmail.com>
2016-12-07 20:36:40 +0000

    Add real endpoint

--- handler.js
+++ handler.js
@@ -2,9 +2,19 @@
 
 let funker = require("./funker-node");
 let sample = require("./sampleresponse.json");
+var request = require("request");
 
 funker.handle(function(args, callback) {
   console.log(args);
+  var ops = {
+    json: true,
+    uri: "http://api.open-notify.org/astros.json"
+  };
 
-  callback(sample);
+  request.get(ops, (err, res, body) => {
+    var speechOutput = "There's currently " + body.number + " people in space";
+    sample.response.outputSpeech.text = speechOutput;
+    sample.card.content = speechOutput;
+    callback(sample);
+  });
 });
