c8ebfd6e83d212b5c270916cf8f1e7b2f2bb8c5c
Alex Ellis <alexellis2@gmail.com>
2016-12-08 09:09:00 +0000

    Stateful - i.e. container persists between requests

--- handler.js
+++ handler.js
@@ -3,19 +3,22 @@
 let funker = require("./funker-node");
 let sample = require("./sampleresponse.json");
 var request = require("request");
+let async = require('async');
 
-funker.handle(function(args, callback) {
-  console.log(args);
-  var ops = {
-    json: true,
-    uri: "http://api.open-notify.org/astros.json"
-  };
-
-  request.get(ops, (err, res, body) => {
-    var speechOutput = "There's currently " + body.number + " people in space";
-    console.log(sample);
-    sample.response.outputSpeech.text = speechOutput;
-    sample.response.card.content = speechOutput;
-    callback(sample);
+async.whilst(() => {console.log("Request.."); return true; },
+(cb) => {
+  funker.handle((args, callback) => {
+    console.log(args);
+    var ops = {
+      json: true,
+      uri: "http://api.open-notify.org/astros.json"
+    };
+    request.get(ops, (err, res, body) => {
+      var speechOutput = "There's currently " + body.number + " people in space";
+      sample.response.outputSpeech.text = speechOutput;
+      sample.response.card.content = speechOutput;
+      callback(sample);
+      cb();
+    });
   });
 });
